ed8e9aa4b27d5f58fac0f42bf0b2bdec8ed8dae6
Juan Diego Sastre Moreno <105946956+DIegoDevcode@users.noreply.github.com>
2022-08-09 12:04:40 -0500

    Agregando la base de la calculadora

---
+++ index.js
@@ -0,0 +1,25 @@
+function suma(){
+ resultado1 =  10 + 23
+  console.log(resultado1)
+}
+
+suma()
+function resta(){
+  resultado2 = 100-75
+  console.log(resultado2)
+}
+resta()
+
+
+function multiplicacion(){
+  resultado3 = 45*90 
+  console.log(resultado3)
+}
+multiplicacion()
+
+function division(){
+  resultado4 = 67/4
+  console.log(resultado4)
+}
+
+division()
